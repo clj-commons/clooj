6e8ac4c602016af563cd5838f324fdbbb807ae55
Arthur Edelstein <arthuredelstein@gmail.com>
2011-01-21 00:41:57 -0800

    first commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clyde "0.0.1-SNAPSHOT"
+  :description "TODO: add summary of your project"
+  :main clyde.core
+  :dependencies [[clojure "1.2.0"]
+                 [clojure-contrib "1.2.0"]])
