cb94081547f2a7aa686c119dceb4f1f38f748d70
arthuredelstein <arthuredelstein@gmail.com>
2011-07-09 02:24:42 -0700

    Edited project.clj via GitHub

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clooj "0.0.3-SNAPSHOT"
-  :description "TODO: add summary of your project"
+  :description "clooj, a small IDE for clojure"
   :main clooj.core
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]])
