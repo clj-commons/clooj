7e8ecf57daacf1781c5c492ed31502da05437a60
Arthur Edelstein <arthuredelstein@gmail.com>
2011-07-31 22:07:55 -0700

    fonts

--- src/clooj/style.clj
+++ src/clooj/style.clj
@@ -1,6 +1,11 @@
 (ns clooj.style
   (:import (javax.swing.text SimpleAttributeSet StyleConstants)
-           (java.awt Color)))
+           (java.awt Color Font FontMetrics GraphicsEnvironment)
+           (java.awt.image BufferedImage)))
+
+(def graphics-object
+  (memoize (fn [] (.createGraphics
+                    (BufferedImage. 1 1 BufferedImage/TYPE_INT_ARGB)))))
 
 (defn set-text-color [text-comp offset length color]
   (let [style-doc (.getStyledDocument text-comp)
@@ -11,3 +16,20 @@
 (defn get-tokens [txt]
   (re-seq #"[:|a-z|A-Z|/|\\|\.|-|0-9|\+\"]+" txt))
 
+;; fonts
+
+(def monospaced?
+  (memoize
+    (fn [font]
+      (let [g (graphics-object)
+            m (.getFontMetrics g font)]
+        (apply == (map #(.charWidth m %) [\m \n \. \M \-]))))))
+
+(defn get-all-fonts-12 []
+  (let [font-names (.. GraphicsEnvironment
+                       getLocalGraphicsEnvironment
+                       getAvailableFontFamilyNames)]
+    (map #(Font. % Font/PLAIN 12) font-names)))
+
+(def get-monospaced-fonts []
+  (filter monospaced? (get-all-fonts-12)))
