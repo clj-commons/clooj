9db6685db042c71f48172ebcfd176bbe68790b92
Arthur Edelstein <arthuredelstein@gmail.com>
2011-09-14 09:31:21 -0700

    license; formatting

---
+++ src/clooj/navigate.clj
@@ -0,0 +1,38 @@
+; Copyright (c) 2011, Arthur Edelstein
+; All rights reserved.
+; Eclipse Public License 1.0
+; arthuredelstein@gmail.com
+
+(ns clooj.navigate
+  (:import (org.fife.ui.rsyntaxtextarea RSyntaxTextArea))
+  (:use [clooj.utils :only (attach-action-keys)]))
+
+(defn get-caret-line-number [comp]
+  (.getLineOfOffset comp (.getCaretPosition comp)))
+
+(defn move-to-doc-start [comp]
+  (.setCaretPosition comp 0))
+
+(defn move-to-doc-end [comp]
+  (.setCaretPosition comp
+    (.. comp getDocument getLength)))
+
+(defn move-to-line-start [comp]
+  (.setCaretPosition comp
+    (.getLineStartOffset comp
+      (get-caret-line-number comp)))) 
+
+(defn move-to-line-end [comp]
+  (.setCaretPosition comp
+    (let [p (.getLineEndOffset comp
+              (get-caret-line-number comp))]
+      (if (= p (.. comp getDocument getLength))
+        p
+        (dec p)))))
+           
+(defn attach-navigation-keys [comp]
+  (attach-action-keys comp
+    ["cmd1 LEFT" #(move-to-line-start comp)]
+    ["cmd1 RIGHT" #(move-to-line-end comp)]
+    ["cmd1 UP" #(move-to-doc-start comp)]
+    ["cmd1 DOWN" #(move-to-doc-end comp)]))
