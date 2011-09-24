6606f1373f31461f94415b66068736dffc05661d
Arthur Edelstein <arthuredelstein@gmail.com>
2011-09-23 20:52:50 -0700

    use clojure-1.3.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clooj "0.1.37"
   :description "clooj, a small IDE for clojure"
   :main clooj.core
-  :dependencies [[clojure "1.3.0-beta3"]]
+  :dependencies [[clojure "1.3.0"]]
   :dev-dependencies [[local-file "0.0.4"]]         
 )
