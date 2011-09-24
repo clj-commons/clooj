a82e620933969ed2e21baeb7f012dab11c6b0b03
Arthur Edelstein <arthuredelstein@gmail.com>
2011-09-23 21:00:51 -0700

    use local-file 0.1.0

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "clooj, a small IDE for clojure"
   :main clooj.core
   :dependencies [[clojure "1.3.0"]]
-  :dev-dependencies [[local-file "0.0.4"]]         
+  :dev-dependencies [[local-file "0.1.0"]]         
 )
