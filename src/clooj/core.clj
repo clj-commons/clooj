f94aa6c127cee2757324e26c3aa102818309108b
Arthur Edelstein <arthuredelstein@gmail.com>
2011-09-24 22:21:11 -0700

    new projects should specify 1.3.0 by default

--- src/clooj/core.clj
+++ src/clooj/core.clj
@@ -558,8 +558,7 @@
 "
 (defproject PROJECTNAME \"1.0.0-SNAPSHOT\"
   :description \"FIXME: write\"
-  :dependencies [[org.clojure/clojure \"1.2.1\"]
-                 [org.clojure/clojure-contrib \"1.2.0\"]])
+  :dependencies [[org.clojure/clojure \"1.3.0\"]])
 "))
       
 (defn specify-source [project-dir title default-namespace]
