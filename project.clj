8cb3e0a143ec84debaccf4158a2ead02980b115d
Arthur Edelstein <arthuredelstein@gmail.com>
2012-04-15 11:52:41 -0700

    use new version of clj-inspector

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "clooj, a small IDE for clojure"
   :main clooj.core
   :dependencies [[clojure "1.3.0"]
-                 [clj-inspector "0.0.8"]
+                 [clj-inspector "0.0.9"]
                  [slamhound "1.2.0"]
                  ;[leiningen-core "2.0.0-SNAPSHOT"]
 ]
