d873107b0568cb6bd21601aa8873c26a924f2cb4
Arthur Edelstein <arthuredelstein@gmail.com>
2012-04-28 21:41:26 -0700

    another bug with arglist fixed by upgrading clj-inspector

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "clooj, a small IDE for clojure"
   :main clooj.core
   :dependencies [[clojure "1.3.0"]
-                 [clj-inspector "0.0.11"]
+                 [clj-inspector "0.0.12"]
                  [slamhound "1.2.0"]
                  ;[leiningen-core "2.0.0-SNAPSHOT"]
                  ]
