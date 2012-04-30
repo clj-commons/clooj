26a53942d50d9fcd2bad51a252d92e0975f21527
Arthur Edelstein <arthuredelstein@gmail.com>
2012-04-29 23:49:17 -0700

    add pomegranate

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[clojure "1.3.0"]
                  [clj-inspector "0.0.12"]
                  [slamhound "1.2.0"]
+                 [com.cemerick/pomegranate "0.0.11"]
                  [com.fifesoft/rsyntaxtextarea "2.0.2"]
                  ]
 )
