7ce6f96287877e39a9e7a5c302bf78de7c336e0e
AtKaaZ <atkaaz@gmail.com>
2013-01-22 15:44:17 +0100

    fix "Could not find artifact clojure:clojure:jar:1.4.0"

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clooj "0.3.11"
   :description "clooj, a small IDE for clojure"
   :main clooj.core
-  :dependencies [[clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-inspector "0.0.12"]
                  [slamhound "1.2.0"]
                  [com.cemerick/pomegranate "0.0.11"]
