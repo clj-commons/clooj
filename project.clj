1fceb3902bedd00248952665226061a0dbd8f175
Arthur Edelstein <arthuredelstein@gmail.com>
2013-03-09 20:51:39 -0800

    migrate to leiningen 2

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject clooj "0.3.11"
   :description "clooj, a small IDE for clojure"
+  :url "https://github.com/arthuredelstein/clooj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main clooj.core
-  :dependencies [[clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-inspector "0.0.12"]
                  [slamhound "1.2.0"]
                  [com.cemerick/pomegranate "0.0.11"]
