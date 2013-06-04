e36c586262f16666799bae6a8477684cbb43d4df
Trevor Wennblom <t@trevorw.tw>
2013-06-04 09:53:38 -0500

    update clojure and nrepl deps

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main clooj.core
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-inspector "0.0.12"]
                  [slamhound "1.2.0"]
                  [com.cemerick/pomegranate "0.0.11"]
                  [com.fifesoft/rsyntaxtextarea "2.0.2"]
-                 [org.clojure/tools.nrepl "0.2.2"]])
+                 [org.clojure/tools.nrepl "0.2.3"]])
