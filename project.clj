7fba05b6ca92c932c04f4fec0d19abf5c9f8d69b
Arthur Edelstein <arthuredelstein@gmail.com>
2013-06-30 11:29:45 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clooj "0.4.3"
+(defproject clooj "0.4.4"
   :description "clooj, a small IDE for clojure"
   :url "https://github.com/arthuredelstein/clooj"
   :license {:name "Eclipse Public License"
