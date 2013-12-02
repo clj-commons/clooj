8f1131138ffebb2898a269f98c8836ff8a2c1574
alhimik45 <alhimik45@gmail.com>
2013-12-02 22:24:30 +0700

    Migrate to last stable Clojure version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main clooj.core
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-inspector "0.0.12"]
                  [slamhound "1.2.0"]
                  [com.cemerick/pomegranate "0.0.11"]
