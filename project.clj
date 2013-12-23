34b1d8099908c67a43021295140fc0e54cf7471c
alhimik45 <alhimik45@gmail.com>
2013-12-23 19:39:16 +0700

    Update RSyntaxTextArea version

--- project.clj
+++ project.clj
@@ -8,5 +8,5 @@
                  [clj-inspector "0.0.12"]
                  [slamhound "1.2.0"]
                  [com.cemerick/pomegranate "0.0.11"]
-                 [com.fifesoft/rsyntaxtextarea "2.0.2"]
+                 [com.fifesoft/rsyntaxtextarea "2.5.0"]
                  [org.clojure/tools.nrepl "0.2.3"]])
