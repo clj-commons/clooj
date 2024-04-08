87d6274d09b34a41883188b8510e3def2901d476
Eero Helenius <eero.helenius@yle.fi>
2024-04-08 13:53:40 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main clooj.core
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-inspector "0.0.12"]
-                 [slamhound "1.2.0"]
-                 [com.cemerick/pomegranate "0.0.11"]
-                 [com.fifesoft/rsyntaxtextarea "2.5.0"]
-                 [org.clojure/tools.nrepl "0.2.3"]])
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [clj-inspector "0.0.16"]
+                 [slamhound "1.5.5"]
+                 [com.cemerick/pomegranate "1.1.0"]
+                 [com.fifesoft/rsyntaxtextarea "3.4.0"]
+                 [org.clojure/tools.nrepl "0.2.13"]])
