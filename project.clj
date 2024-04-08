5b0ad5b7c839ea3d1d214a6569aa732f02d1f2d5
Eero Helenius <eero.helenius@yle.fi>
2024-04-08 14:20:13 +0300

    Fix compatibility with modern JVMs

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main clooj.core
+  :jvm-opts ["--add-exports" "java.desktop/com.apple.eawt=ALL-UNNAMED"]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-inspector "0.0.16"]
                  [slamhound "1.5.5"]
